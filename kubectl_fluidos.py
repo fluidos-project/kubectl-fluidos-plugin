dec4b457fe6aad38134b06693f37e0d36c5de55b
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2023-08-08 09:25:15 +0100

    add placeholder for tests

--- kubectl_fluidos.py
+++ kubectl_fluidos.py
@@ -1,10 +1,23 @@
 #! /usr/bin/env python
 import os
 import sys
+import yaml
+from enum import Enum, auto
+
+class InputFormat(Enum):
+    K8S = auto()
+    MSPL = auto()
+
+
+def _check_input_format(input_data) -> InputFormat:
+    return None 
+
 
 def main() -> int:
     print(sys.argv)
-    
+
+
+
 
     return os.system("kubectl apply " + " ".join(sys.argv[1:]))
 
