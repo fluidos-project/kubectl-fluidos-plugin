7a89a6691089ca806066d831c0b4c8a92bc474eb
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2023-08-08 09:36:46 +0100

    add basic structure

--- kubectl_fluidos.py
+++ kubectl_fluidos.py
@@ -1,24 +1,59 @@
 #! /usr/bin/env python
+import logging
 import os
 import sys
 import yaml
 from enum import Enum, auto
+
 
 class InputFormat(Enum):
     K8S = auto()
     MSPL = auto()
 
 
-def _check_input_format(input_data) -> InputFormat:
-    return None 
+
+def _is_YAML(data: str) -> bool:
+    raise NotImplementedError()
+
+
+def _is_XML(data: str) -> bool:
+    raise NotImplementedError()
+
+
+def _check_input_format(input_data: str) -> InputFormat:
+    if _is_YAML(input_data):
+        return InputFormat.K8S
+    elif _is_XML(input_data):
+        return InputFormat.MSPL
+    raise ValueError("Unkown format")
+
+
+def _has_intent_defined(input_data: str) -> bool:
+    return NotImplementedError
+
+
+def _extract_input_data(arguments: list[str]) -> str:
+    raise NotImplementedError
 
 
 def main() -> int:
-    print(sys.argv)
+    logging.info("Starting FLUIDOS kubectl extension")
+
+    data = _extract_input_data(sys.argv)
+
+    logging.trace(f"Extracted:\n---\n{data}\n---")
 
 
+    input_format = _check_input_format(data)
 
+    if input_format == InputFormat.MSPL:
+        # INVOKE MSPL orchestrator
+        raise NotImplementedError()
+    elif input_format == InputFormat.K8S:
+        if _has_intent_defined(data):
+            raise NotImplementedError()
 
+    # if nothing else applies, fallback to vanilla kubectl apply behavior
     return os.system("kubectl apply " + " ".join(sys.argv[1:]))
 
 
