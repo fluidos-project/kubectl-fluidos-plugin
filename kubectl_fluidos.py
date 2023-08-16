3523ee55a91d18bed9c9e2b1ef19d505ea737c0a
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2023-08-16 11:58:45 +0100

    Initial version, with testing

--- kubectl_fluidos.py
+++ kubectl_fluidos.py
@@ -2,7 +2,15 @@
 import logging
 import os
 import sys
+from typing import Any, Callable, Optional, TextIO
 import yaml
+
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
+
+from xml.etree import ElementTree
 from enum import Enum, auto
 
 
@@ -11,51 +19,126 @@ class InputFormat(Enum):
     MSPL = auto()
 
 
+INTENT_K8S_KEYWORD = "quality_intent"
+
+
 
 def _is_YAML(data: str) -> bool:
-    raise NotImplementedError()
+    try:
+        _ = _to_YAML(data)
+        return True
+    except Exception as e:
+        logging.info(str(e))
+    return False
 
 
 def _is_XML(data: str) -> bool:
+    try:
+        _ = ElementTree.fromstring(data)
+        return True
+    except Exception as e:
+        logging.info(str(e))
+    return False
+
+
+def _to_YAML(data: str) -> dict[str, Any]:
+    return yaml.load(data, Loader=Loader)
+
+
+def _check_input_format(input_data: str) -> (InputFormat, Optional[dict[str, Any]]):
+    if _is_XML(input_data):
+        return (InputFormat.MSPL, None)
+    elif _is_YAML(input_data):
+        return (InputFormat.K8S, _to_YAML(input_data))
+    raise ValueError("Unknown format")
+
+
+def _has_container_intent_defined(container_template: dict[str, Any]) -> bool:
+    return "resources" in container_template and INTENT_K8S_KEYWORD in container_template["resources"]
+
+
+def _has_intent_defined(data: dict[str, Any]) -> bool:
+    # big assumption, of where intents are specified within a deployment
+    for container_template in data["spec"]["template"]["spec"]["containers"]:
+        # assume it is specified as resource request
+        if _has_container_intent_defined(container_template):
+            return True
+
+    return False
+
+
+def _read_file_argument_content(filename: str) -> str:
+    with open(filename) as input_file:
+        return input_file.read()
+
+
+def _attempt_reading_from_stdio(stdin: TextIO) -> str:
+    return stdin.read()
+
+
+def _extract_input_data(arguments: list[str], stdin: TextIO) -> tuple[list[str], Optional[str]]:
+    input_data: list[str] = [
+        _read_file_argument_content(arguments[idx + 1]) for idx, arg in enumerate(arguments) if (arg == "-f" or arg == "--filename") and idx + 1 < len(arguments)
+    ]
+
+    if len(input_data):
+        return input_data
+
+    stdin_data = _attempt_reading_from_stdio(stdin)
+
+    if len(stdin_data):
+        return [stdin_data]
+
+    return None
+
+
+def _is_deployment(spec: dict[str, Any]) -> bool:
+    if type(spec) == dict:
+        return spec.get("kind", None) == "Deployment"
+    return False
+
+
+def _behavior_not_defined() -> int:
     raise NotImplementedError()
 
 
-def _check_input_format(input_data: str) -> InputFormat:
-    if _is_YAML(input_data):
-        return InputFormat.K8S
-    elif _is_XML(input_data):
-        return InputFormat.MSPL
-    raise ValueError("Unkown format")
+def _default_apply(args: list[str], stdin: bytes) -> int:
+    return os.system("kubectl apply " + " ".join(args))
 
 
-def _has_intent_defined(input_data: str) -> bool:
-    return NotImplementedError
-
-
-def _extract_input_data(arguments: list[str]) -> str:
-    raise NotImplementedError
-
-
-def main() -> int:
+def fluidos_kubectl_extension(argv: list[str], stdin: TextIO, *, on_apply: Callable[..., int] = _default_apply, on_mlps: Callable[..., int] = _behavior_not_defined, on_k8s_w_intent: Callable[..., int] = _behavior_not_defined) -> int:
     logging.info("Starting FLUIDOS kubectl extension")
 
-    data = _extract_input_data(sys.argv)
+    data = _extract_input_data(argv, stdin) # this needs to be fixed, we cannot assume kubectl apply is receiving data from stdin if it has been consumed here
 
-    logging.trace(f"Extracted:\n---\n{data}\n---")
+    if 0 < len(data) < 2:
+        # we assume to handle only one file/spec, for the moment at least
+        try:
+            input_format, spec = _check_input_format(data[0])
 
+            if input_format == InputFormat.MSPL:
+                # INVOKE MSPL orchestrator
+                logging.info("Invoking MSPL Service Handler")
+                return on_mlps(sys.argv[-1:])
+            elif input_format == InputFormat.K8S:
+                if _is_deployment(spec) and _has_intent_defined(spec):
+                    logging.info("Invoking K8S with Intent Service Handler")
+                    return on_k8s_w_intent(sys.argv[-1:])
 
-    input_format = _check_input_format(data)
-
-    if input_format == InputFormat.MSPL:
-        # INVOKE MSPL orchestrator
-        raise NotImplementedError()
-    elif input_format == InputFormat.K8S:
-        if _has_intent_defined(data):
-            raise NotImplementedError()
+        except ValueError:
+            logging.info("Unknown format, fallback to apply")
+    else:
+        logging.info("Skipping because multiple specification available")
 
     # if nothing else applies, fallback to vanilla kubectl apply behavior
-    return os.system("kubectl apply " + " ".join(sys.argv[1:]))
+    logging.info("Invoking kubectl apply")
+    return on_apply(sys.argv[-1:], spec)
 
+
+def main():
+    raise SystemExit(
+        fluidos_kubectl_extension(sys.argv, sys.stdin)
+    )
 
 if __name__ == "__main__":
-    raise SystemExit(main())
+    main()
---
+++ tests/test_kubectl_fluidos.py
@@ -0,0 +1,133 @@
+import codecs
+import pkg_resources
+import pytest
+from io import StringIO
+from pytest import fail
+
+from kubectl_fluidos import fluidos_kubectl_extension, _is_XML, _is_YAML
+
+
+def test_xml_validation():
+    xml = """<?xml version="1.0"?>
+<data>
+    <country name="Liechtenstein">
+        <rank>1</rank>
+        <year>2008</year>
+        <gdppc>141100</gdppc>
+        <neighbor name="Austria" direction="E"/>
+        <neighbor name="Switzerland" direction="W"/>
+    </country>
+    <country name="Singapore">
+        <rank>4</rank>
+        <year>2011</year>
+        <gdppc>59900</gdppc>
+        <neighbor name="Malaysia" direction="N"/>
+    </country>
+    <country name="Panama">
+        <rank>68</rank>
+        <year>2011</year>
+        <gdppc>13600</gdppc>
+        <neighbor name="Costa Rica" direction="W"/>
+        <neighbor name="Colombia" direction="E"/>
+    </country>
+</data>
+"""
+
+    assert _is_XML(xml)
+    assert not _is_XML(None)
+    assert not _is_XML("")
+
+    text = """grandparent:
+  parent:
+    child:
+      name: Bobby
+    sibling:
+      name: Molly
+"""
+    assert not _is_XML(text)
+
+
+def test_validate_yaml():
+    text = """grandparent:
+  parent:
+    child:
+      name: Bobby
+    sibling:
+      name: Molly
+"""
+    assert _is_YAML(text)
+    assert not _is_YAML(None)
+
+
+def test_input_defined_as_stdin():
+    with pkg_resources.resource_stream(__name__, "dataset/test-deployment-single.yaml") as stream:
+        text = codecs.getreader("utf-8")(stream).read()
+
+    def validation(a, b):
+        return 123456
+
+    return_value = fluidos_kubectl_extension(["kubectl-fluidos"], StringIO(text), on_apply=validation)
+
+    assert return_value == 123456
+
+
+def test_input_from_parameter():
+    doc_file = pkg_resources.resource_filename(__name__, "dataset/test-deployment-single.yaml")
+
+    def validation(a, b):
+        return 123456
+
+    return_value = fluidos_kubectl_extension(["kubectl-fluidos", "-f", doc_file], StringIO(), on_apply=validation)
+
+    assert return_value == 123456
+    
+
+def test_validate_fallsback_to_appy():
+    doc_file = pkg_resources.resource_filename(__name__, "dataset/test-deployment-single.yaml")
+
+    def apply(a, b):
+        return 123456
+
+    def drl(a, b):
+        return 9876342
+
+    def mspl(a, b):
+        return 000000
+
+    return_value = fluidos_kubectl_extension(["kubectl-fluidos", "-f", doc_file], StringIO(), on_apply=apply, on_k8s_w_intent=drl, on_mlps=mspl)
+
+    assert return_value == 123456
+
+
+def test_yaml_with_intent_invokes_drl():
+    doc_file = pkg_resources.resource_filename(__name__, "dataset/test-deployment-single-w-intent.yaml")
+
+    def apply(a, b):
+        return 123456
+
+    def drl(a):
+        return 9876342
+
+    def mspl(a):
+        return 000000
+
+    return_value = fluidos_kubectl_extension(["kubectl-fluidos", "-f", doc_file], StringIO(), on_apply=apply, on_k8s_w_intent=drl, on_mlps=mspl)
+
+    assert return_value == 9876342
+
+
+def test_xml_invokes_mspl_by_default():
+    doc_file = pkg_resources.resource_filename(__name__, "dataset/test-mspl.xml")
+
+    def apply(a, b):
+        return 123456
+
+    def drl(a):
+        return 9876342
+
+    def mspl(a):
+        return 000000
+
+    return_value = fluidos_kubectl_extension(["kubectl-fluidos", "-f", doc_file], StringIO(), on_apply=apply, on_k8s_w_intent=drl, on_mlps=mspl)
+
+    assert return_value == 000000
