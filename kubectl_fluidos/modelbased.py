859b89a86c0d812807229e1a12e24ddb3b34edd2
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2023-10-04 10:51:24 +0100

    remove warnings
    Signed-off-by: Stefano Braghin <527806+stefano81@users.noreply.github.com>

--- kubectl_fluidos/modelbased.py
+++ kubectl_fluidos/modelbased.py
@@ -1,8 +1,30 @@
+# coding: utf-8
+'''
+------------------------------------------------------------------------------
+Copyright 2023 IBM Research
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+------------------------------------------------------------------------------
+'''
+
 from __future__ import annotations
 
 from dataclasses import dataclass
 from logging import Logger
+from kubernetes import config
+from kubernetes.client import Configuration
+from kubernetes.config import ConfigException
 
+from kubectl_fluidos.common import k8sArgParser
 
 logger = Logger(__name__)
 
@@ -13,7 +35,29 @@ class ModelBasedOrchestratorConfiguration:
 
     @staticmethod
     def build_configuration(args: list[str]) -> ModelBasedOrchestratorConfiguration:
-        pass
+        try:
+            k8s_args, remaining_args = k8sArgParser().parse_known_args(args)
+            # missing expanding load configuration from provided command line options
+
+            loading_args = {}
+
+            if k8s_args.kubeconfig:
+                loading_args["config_file"] = k8s_args.kubeconfig
+
+            config.load_config(**loading_args)
+
+            try:
+                c = Configuration().get_default_copy()
+            except AttributeError:
+                c = Configuration()
+                c.assert_hostname = False
+            Configuration.set_default(c)
+
+            return ModelBasedOrchestratorConfiguration()
+        except ConfigException:
+            print("Nothing to do here")
+
+        raise RuntimeError("Unable to build configuration")
 
 
 class ModelBasedOrchestratorProcessor:
